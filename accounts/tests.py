2b0da894eb64e7e68985a0fa4a87e162ae2b99b5
Giselle <gigi108@gmail.com>
2018-09-24 15:06:56 +0000

    pushing to heroku"

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from .forms import ProfileForm
 
 # Create your tests here.
+class TestDjango(TestCase):
+    
+    def test_is_this_thing_on(self):
+        self.assertEqual(1, 1)
+        
+
+    def test_profile_form_valid(self):
+        form = ProfileForm({
+            'first_name': 'daisy',
+            'last_name': 'duck',
+            'profile_image': 'avatars/anonymous.png',
+            'gender': 'FEMALE',
+            'house_no': '55',
+            'street_name': 'simple street',
+            'town_or_city': 'norfolk',
+            'postcode': 'nxr45',
+            'county': 'berkshire',
+            'country': 'england',
+        })
+ 
+        self.assertTrue(form.is_valid())
